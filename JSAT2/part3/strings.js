9e9df9e4f5755d3c52bd847af0bd4282d25e79cc
Finnley Newnham <162288636+finnleyjasper@users.noreply.github.com>
2026-08-27 14:28:07 +1000

    feat: change myName to upper and lowercase

--- JSAT2/part3/strings.js
+++ JSAT2/part3/strings.js
@@ -2,7 +2,7 @@
 const myString = "This is a string";
 const anotherString = "   Another string";
 const hello = "Hello there!";
-const myName = "Finnley";
+let myName = "Finnley";
 
 // Print strings to console
 console.log("The strings created are: " + myString + ", " + anotherString + ", " + hello + ", " + myName);
@@ -18,9 +18,20 @@ console.log("The first character of myString is:", firstCharacter);
 console.log("The 11th character of myString is:", eleventhCharacter);
 
 // Create new strings using slice() and substring()
+// Both methods extract a part of the string and return it as a new string. The difference is that slice() can accept negative indices, while substring() cannot!
 const slicedString = myString.slice(5, 9); // results in "is a"
-const substringString = anotherString.substring(6, 9); // results in "Another"
+const substringString = anotherString.substring(6, 9); // results in "the"
 
 // Print the new strings to the console
 console.log("The sliced string is:", slicedString);
 console.log("The substring is:", substringString);
+
+// Create uppercase version of myName
+myName = myName.toUpperCase();
+// Print the uppercase version of myName to the console
+console.log("myName in uppercase is:", myName);
+
+// Create lowercase version of myName
+myName = myName.toLowerCase();
+// Print the lowercase version of myName to the console
+console.log("myName in lowercase is:", myName);
