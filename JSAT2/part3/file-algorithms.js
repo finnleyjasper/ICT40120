8d9713625cc86ffac51b85104ad678fc9b2c7fa7
Finnley Newnham <162288636+finnleyjasper@users.noreply.github.com>
2026-08-27 16:05:34 +1000

    feat: add file reading/writing algorithms

---
+++ JSAT2/part3/file-algorithms.js
@@ -0,0 +1,23 @@
+/**
+ * Node.js's built-in file system tools.
+ */
+const fs = require("fs");
+
+/**
+ * Saves a string to a text file.
+ * @param {string} fileName - The name or path of the file to save.
+ * @param {string} text - The text to write to the file.
+ * @returns {void}
+ */
+function saveStringToFile(fileName, text) {
+  fs.writeFileSync(fileName, text, "utf8");
+}
+
+/**
+ * Reads and returns the contents of a text file.
+ * @param {string} fileName - The name or path of the file to read.
+ * @returns {string} The text read from the file.
+ */
+function readTextFile(fileName) {
+  return fs.readFileSync(fileName, "utf8");
+}
