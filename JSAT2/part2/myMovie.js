d199a02e46c28812d8375c7dc6964693520fbe35
Finnley Newnham <162288636+finnleyjasper@users.noreply.github.com>
2026-08-27 14:00:04 +1000

    feat: add properties to myMovie

--- JSAT2/part2/myMovie.js
+++ JSAT2/part2/myMovie.js
@@ -6,3 +6,10 @@ const myMovie = {
 
 // Print the myMovie object to the console
 console.log("myMovie object:", myMovie);
+
+// Add new properties to the existing myMovie object
+myMovie.rating = 5;
+myMovie.summary = "World War II movie about the Normandy landings";
+
+// Print the myMovie object to the console with the new properties
+console.log("myMovie object:", myMovie);
