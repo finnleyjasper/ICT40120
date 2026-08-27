b937d992e7034a92825d82b550f4efd260c7b7b4
Finnley Newnham <162288636+finnleyjasper@users.noreply.github.com>
2026-08-27 13:56:49 +1000

    feat: create myMovie object

---
+++ JSAT2/part2/myMovie.js
@@ -0,0 +1,8 @@
+// Create the myMovie object as an object literal
+const myMovie = {
+  title: "The longest day",
+  year: 1964
+};
+
+// Print the myMovie object to the console
+console.log("myMovie object:", myMovie);
