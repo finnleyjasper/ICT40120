34da5c3f9929112ef62bd9d35250e50afb5fbe7d
Finnley Newnham <162288636+finnleyjasper@users.noreply.github.com>
2026-08-27 14:13:51 +1000

    feat: add strings

---
+++ JSAT2/part3/strings.js
@@ -0,0 +1,8 @@
+// Define strings
+const myString = "This is a string";
+const anotherString = "   Another string";
+const hello = "Hello there!";
+const myName = "Finnley";
+
+// Print strings to console
+console.log("The strings created are: " + myString + ", " + anotherString + ", " + hello + ", " + myName);
