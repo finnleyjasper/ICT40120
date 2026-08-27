e84986f320e0bd06adde312c0b382aa56d7ee808
Finnley Newnham <162288636+finnleyjasper@users.noreply.github.com>
2026-08-27 14:17:34 +1000

    feat: use string utility functions

--- JSAT2/part3/strings.js
+++ JSAT2/part3/strings.js
@@ -6,3 +6,13 @@ const myName = "Finnley";
 
 // Print strings to console
 console.log("The strings created are: " + myString + ", " + anotherString + ", " + hello + ", " + myName);
+
+// Use utility functions to find relevant information
+const myStringLength = myString.length;
+const firstCharacter = myString.charAt(0); // Index starts at 0
+const eleventhCharacter = myString.charAt(10); // Index starts at 0
+
+// Print all the information to the console
+console.log("The length of myString is:", myStringLength);
+console.log("The first character of myString is:", firstCharacter);
+console.log("The 11th character of myString is:", eleventhCharacter);
